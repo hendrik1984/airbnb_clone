a62a5d4e93debfc675ce86ca5bcb111f5386c987
hendrik <li.zhiqiang2011@gmail.com>
2025-07-15 14:21:17 +0700

    disable debug console

--- app/javascript/controllers/navbar_controller.js
+++ app/javascript/controllers/navbar_controller.js
@@ -2,12 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    console.log("connected navbar!")
+    // console.log("connected navbar!")
   }
   
   toggle() {
     const div = document.getElementById("menu-dropdown-items");
-    console.log(div);
+    // console.log(div);
     div.classList.toggle('hidden');
   }
 }
